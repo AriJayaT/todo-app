2f8ca487bea6c18b9f9355f49bf380597a13a84c
Ari Jaya Teguh <arijayateguh@gmail.com>
2025-06-05 20:20:38 +0700

    deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import axios from 'axios';
 
 // Configure axios defaults
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
